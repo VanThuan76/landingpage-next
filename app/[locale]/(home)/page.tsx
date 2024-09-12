5ae6ba7732a17daba4947017e0e1ea4449043d6c
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-09-12 19:33:41 +0700

    #fix: thuanvv fix

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -13,9 +13,6 @@ import SparklesText from "@/shared/components/ui/sparkles-text";
 import Pulsating from "@/shared/components/ui/pulsating";
 
 export default function Home() {
-    const [intervalDuration, setIntervalDuration] = useState(3000);
-    const index = useRef(0);
-
     const confettiRef = useRef<ConfettiRef>(null);
 
     function handleLink() {
@@ -44,16 +41,16 @@ export default function Home() {
                     className="inline-flex w-[60%] h-16 animate-shimmer items-center justify-center rounded-full border-none bg-[linear-gradient(110deg,#f53d3d,45%,#ff9a9a,55%,#f53d3d)] bg-[length:200%_100%] px-2 font-medium text-slate-400 transition-colors shadow-[0_4px_8px_rgba(255,215,0,0.5)] focus:outline-none focus:ring-2 focus:ring-[#ffd700] focus:ring-offset-2 focus:ring-offset-slate-50"
                     onClick={() => handleLink()}
                 >
-                    <TextHoverEffect text="ĐĂNG KÝ NGAY" className="text-4xl stroke-amber-700 fill-yellow-300" className2="text-4xl stroke-amber-700" height="100%" />
+                    <TextHoverEffect text="ĐĂNG KÝ NHẬN NGAY" className="text-4xl stroke-amber-700 fill-yellow-300" className2="text-4xl stroke-amber-700" height="100%" />
                 </motion.button>
 
                 <TextHoverEffect text="+ 59K" className="text-8xl stroke-white fill-red-600" className2="text-8xl stroke-red-900" strokeWidth="4" strokeWidth2="5" height="30%" />
 
-                <Pulsating className="px-2 border-2 border-blue-400" >
+                {/* <Pulsating className="px-2 border-2 border-blue-400" >
                     <SparklesText>
                         <TextHoverEffect text="THƯỞNG NẠP +1500k" className="stroke-violet-300 fill-white" className2="stroke-black" strokeWidth="1" strokeWidth2="2" height="100%" />
                     </SparklesText>
-                </Pulsating>
+                </Pulsating> */}
 
                 <div className="relative p-2 mt-10 bg-red-500 rounded-lg" onClick={() => handleJoinTele()}>
                     <PhoneCall className="absolute w-10 h-10 text-white top-3 left-2" />
