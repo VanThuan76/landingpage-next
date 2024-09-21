82b864d16555cd6cae884ea2294f98f2ad1d5625
Austin Vu <88172491+VanThuan76@users.noreply.github.com>
2024-09-21 16:31:18 +0700

    Update not-found.tsx

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -3,7 +3,7 @@ function NotFound() {
         <div className="flex flex-col items-center justify-center w-full h-screen py-24">
             <h1 className="text-6xl font-bold ">
                 <span className="text-destructive">404</span>, Page{" "}
-                <span className="text-destructive">Not Found!</span>
+                <span className="text-destructive">Không tìm thấy!</span>
             </h1>
         </div>
     );
